00b6005186f185d9d05ba93caada919df7727b99
Elindanova <bonamy.antoine@hotmail.fr>
2023-04-07 15:38:33 +0200

    ticket poke14 done : Clean React App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import Home from "./pages/Home";
-
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <p>POKEWORLD LOADING............</p>
     </div>
   );
 }
