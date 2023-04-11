c3e83970498a142afd58a70f196c003d0391237f
chloeafp <chloeafp@gmail.com>
2023-04-11 17:22:21 +0200

    link components dans App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,9 @@
+import Header from "./components/Header";
+import Filtres from "./components/Filtres";
+import Deck from "./components/Deck";
+import Intro from "./components/Intro";
+
+
 function App() {
   return (
     <div className="App">
---
+++ frontend/src/components/Filtres.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Filtres = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Filtres;
