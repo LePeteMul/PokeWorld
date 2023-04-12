a36f49f81f47e073559ad6039327cc2459b99b5d
LePeteMul <petemul@hotmail.com>
2023-04-12 15:38:15 +0200

    premier filtre sans style et tableau placeholder

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const Intro = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+function Intro() {
+  return <div />;
+}
 
 export default Intro;
