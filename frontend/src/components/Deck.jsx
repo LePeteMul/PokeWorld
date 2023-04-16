3aedacbc17fa2b25277e975ff24b55064373c1e4
chloeafp <chloeafp@gmail.com>
2023-04-16 18:48:43 +0200

    affiche pokemon on Deck / Card not fonctional tho

--- frontend/src/components/Deck.jsx
+++ frontend/src/components/Deck.jsx
@@ -1,14 +1,41 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import DeckList from "./DeckList";
 
 function Deck() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const getPokemon = () => {
+    const allPokemon = [];
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i < 152; i++) {
+      allPokemon.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+    axios
+      .all(allPokemon.map((poke) => axios.get(poke)))
+      .then((res) => setPokemon(res));
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <div className="Deck">
       <div className="deckList">
         <SearchBar />
-        <DeckList />
+        <div className="pokemonList">
+          {pokemon.map((poke) => (
+            <div>
+              <DeckList
+                name={poke.data.name}
+                image={poke.data.sprites.other.dream_world.front_default}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/DeckList.jsx
+++ frontend/src/components/DeckList.jsx
@@ -1,39 +1,21 @@
-import React, { useEffect, useState } from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import axios from "axios";
-import Cards from "./Cards";
+import React from "react";
 
-function DeckList() {
-  const [pokemonImg, setPokemonImg] = useState();
-  const [pokemonName, setPokemonName] = useState();
-  const [isShown, setIsShown] = useState(false);
+// eslint-disable-next-line react/prop-types
+export default function DeckList({ name, image }) {
+  /*   const [isShown, setIsShown] = useState(false);
 
   const handleClickCardShown = () => {
     setIsShown((current) => !current);
-  };
-
-  useEffect(() => {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/charizard`)
-      .then((res) => {
-        // eslint-disable-next-line no-restricted-syntax
-        console.log(res.data);
-        setPokemonImg(res.data.sprites.other.dream_world.front_default);
-        setPokemonName(res.data.name);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+  }; */
 
   return (
     <div className="DeckList">
-      <button onClick={handleClickCardShown} type="button">
-        <img src={pokemonImg} alt={pokemonName} />
+      <img src={image} alt={name} /> <br />
+      {name}
+      {/*    <button onClick={handleClickCardShown} type="button">
+        <img src={image} alt={name} />
       </button>
-      {isShown && <Cards />}
+      {isShown && <Cards />} */}
     </div>
   );
 }
-
-export default DeckList;
