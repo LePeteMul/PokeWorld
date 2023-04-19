11c4f3763cb2e3b3c72a3b56ff0ddae25dce497b
chloeafp <chloeafp@gmail.com>
2023-04-19 14:19:45 +0200

    search bar interactive

--- frontend/src/components/Deck/Deck.jsx
+++ frontend/src/components/Deck/Deck.jsx
@@ -1,46 +1,47 @@
 import React, { useState, useEffect } from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import axios from "axios";
 import SearchBar from "./SearchBar";
 import DeckList from "./DeckList";
 
 export default function Deck() {
   const [pokemon, setPokemon] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState(pokemon);
 
   const getPokemon = () => {
     const allPokemon = [];
-    // eslint-disable-next-line no-plusplus
-    for (let i = 1; i < 152; i++) {
+    for (let i = 1; i < 152; i += 1) {
       allPokemon.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
     axios
       .all(allPokemon.map((poke) => axios.get(poke)))
-      .then((res) => setPokemon(res));
+      .then((res) => setPokemon(res))
+      .then(() => console.warn(pokemon));
   };
 
   useEffect(() => {
     getPokemon();
   }, []);
 
-  const handlePokemonSearch = (name) => {
-    const filteredPokemon = [];
-    if (name === "") {
-      getPokemon();
-    }
-    for (const i in pokemon) {
-      if (pokemon[i].data.name.includes(name)) {
-        filteredPokemon.push(pokemon[i]);
-      }
-    }
-    setPokemon(filteredPokemon);
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
   };
+
+  useEffect(() => {
+    setFilter(
+      pokemon.filter((el) =>
+        el.data.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, pokemon]);
 
   return (
     <div className="Deck">
       <div className="deckList">
-        <SearchBar handlePokemonSearch={handlePokemonSearch} />
+        <SearchBar handleSearchChange={handleSearchChange} />
+
         <div className="pokemonList">
-          {pokemon.map((poke) => (
+          {filter.map((poke) => (
             <div>
               <DeckList
                 name={poke.data.name}
