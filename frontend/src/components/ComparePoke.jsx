4dcbc48f4bde6ffdc7a23d54ba6fd8557f670b8d
Mathieu Mrt <martinmathieu49@gmail.com>
2023-04-26 10:51:30 +0200

    Poke29

---
+++ frontend/src/components/ComparePoke.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Deck from "@components/Deck/Deck";
+import Header from "@components/Header";
+
+function ComparePoke() {
+  return (
+    <div>
+      <Header />
+      <section id="main-section">
+        <Deck />
+      </section>
+    </div>
+  );
+}
+
+export default ComparePoke;
