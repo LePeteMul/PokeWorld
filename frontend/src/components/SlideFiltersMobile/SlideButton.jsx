dcf796b5e4741a4a80abf3f8b2d32f7b7bcc4aa5
Elindanova <bonamy.antoine@hotmail.fr>
2023-04-26 10:50:56 +0200

    Create Slide button

--- frontend/src/components/SlideFiltersMobile/SlideButton.jsx
+++ frontend/src/components/SlideFiltersMobile/SlideButton.jsx
@@ -1,6 +1,7 @@
+import PropTypes from "prop-types";
 import pokeballButton from "../../assets/Bouton_pokeball.png";
 
-function SlideButton({slideIsOpened, setSlideIsOpened}) {
+function SlideButton({ slideIsOpened, setSlideIsOpened }) {
   const toggleMenu = () => {
     setSlideIsOpened((current) => !current);
     console.info(slideIsOpened);
@@ -18,5 +19,10 @@ function SlideButton({slideIsOpened, setSlideIsOpened}) {
     </div>
   );
 }
+
+SlideButton.propTypes = {
+  slideIsOpened: PropTypes.bool.isRequired,
+  setSlideIsOpened: PropTypes.func.isRequired,
+};
 
 export default SlideButton;
--- frontend/src/components/SlideFiltersMobile/SlideFilters.jsx
+++ frontend/src/components/SlideFiltersMobile/SlideFilters.jsx
@@ -1,17 +1,20 @@
+import PropTypes from "prop-types";
 import SlideButton from "./SlideButton";
 
-function SlideFilters({slideIsOpened, setSlideIsOpened}) {
+function SlideFilters({ slideIsOpened, setSlideIsOpened }) {
   return (
     <div className="slide-filters-box">
-      <div>
-        
-      </div>
+      <div className="" />
       <SlideButton
         slideIsOpened={slideIsOpened}
         setSlideIsOpened={setSlideIsOpened}
       />
-   </div>
+    </div>
   );
 }
 
+SlideFilters.propTypes = {
+  slideIsOpened: PropTypes.bool.isRequired,
+  setSlideIsOpened: PropTypes.func.isRequired,
+};
 export default SlideFilters;
