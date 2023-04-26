f96f4d588196857735f31dbdb93edb2bc1a44373
chloeafp <chloeafp@gmail.com>
2023-04-26 12:08:27 +0200

    localstorage favorite

--- frontend/src/components/Deck/DeckList.jsx
+++ frontend/src/components/Deck/DeckList.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import * as React from "react";
 import PropTypes from "prop-types";
 import Cards from "../Cards";
 
 export default function DeckList({ name, image, id }) {
   const [isShown, setIsShown] = useState(false);
-  const [favorite, setFavorite] = useState(false);
+  const [favorite, setFavorite] = useState(
+    JSON.parse(localStorage.getItem("is-fav")) !== null
+      ? JSON.parse(localStorage.getItem("is-fav")).includes(id)
+      : false
+  );
 
   const handleClickCardShown = () => {
     setIsShown((current) => !current);
   };
 
   const handleClickFavorite = () => {
+    if (!Array.isArray(JSON.parse(localStorage.getItem("is-fav")))) {
+      localStorage.setItem("is-fav", JSON.stringify([]));
+    }
+
+    if (favorite) {
+      const actualFavorites = JSON.parse(localStorage.getItem("is-fav"));
+      const index = actualFavorites.findIndex((pokemon) => pokemon === id);
+      actualFavorites.splice(index);
+      localStorage.setItem("is-fav", JSON.stringify(actualFavorites));
+    } else {
+      const actualFavorites = JSON.parse(localStorage.getItem("is-fav"));
+      actualFavorites.push(id);
+      localStorage.setItem("is-fav", JSON.stringify(actualFavorites));
+    }
     setFavorite(!favorite);
   };
 
