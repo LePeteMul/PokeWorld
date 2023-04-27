0c17399af945455f3b16cbc3e45469ffb68925c7
unknown <emilie.nathan@gmail.com>
2023-04-27 10:26:15 +0200

    lien favicone reparé

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
-import ParentsLinks from "@components/ParentsLinks";
+import ParentsLinks from "./components/ParentsLinks";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
