f0160335e0d48ae9a71b7ecc9befc670a728eaef
LePeteMul <petemul80@gmail.com>
2023-05-04 10:53:59 +0200

    poke54 et 53 css bug fixes sur menus desktop et mobile

--- frontend/src/components/Comparator/IntroComparator.jsx
+++ frontend/src/components/Comparator/IntroComparator.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../../styles/MyDeck/_introMyDeck.scss";
 
 function IntroComparator() {
@@ -14,21 +13,6 @@ function IntroComparator() {
           With your personalized deck, you're ready to take on any opponents and
           become a true <span className="bold">Pokemon master</span>.
         </p>
-      </div>
-      <div>
-        <nav className="navBar">
-          <ul>
-            <li>
-              <Link to="/AppPokeworld">Home</Link>
-            </li>
-            <li>
-              <Link to="/MyDeck">My Deck</Link>
-            </li>
-            <li>
-              <Link to="/Comparator">Comparator</Link>
-            </li>
-          </ul>
-        </nav>
       </div>
     </div>
   );
